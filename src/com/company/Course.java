25a04d142c4920edf445c774b1ba02c2aba8d462
Jikke1995 <jikke.vdende@hotmail.com>
2016-02-18 18:14:36 +0100

    Vakken aangemaakt (courses)

---
+++ src/com/company/Course.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Course {
+    String name;
+
+    public Course (String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,18 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<>();
+        ArrayList<Course> courses = new ArrayList<>();
 
         // while je nog kan lezen:
             try {
-            BufferedReader Lesrooster =
+            BufferedReader Studentinfo =
                     new BufferedReader (new FileReader("resources/StudentsFile.csv"));
             while(true) {
-                String name = Lesrooster.readLine();
+                String name = Studentinfo.readLine();
                 if (name == null) break;
                 students.add(new Student(name.split(",")[0] + ", " + name.split(",")[1], name.split(",")[2]));
             }
-            Lesrooster.close();
+            Studentinfo.close();
         } catch (IOException ex) {
                 System.out.println("Can't find file");
                 System.exit(1);
