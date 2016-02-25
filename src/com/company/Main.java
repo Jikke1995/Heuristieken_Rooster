bdeb4726894b80b057231686e2fe312db8f1a448
Jikke1995 <jikke.vdende@hotmail.com>
2016-02-25 19:28:07 +0100

    Ik heb class Room toegevoegd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<>();
         ArrayList<Course> courses = new ArrayList<>();
+        ArrayList<Room> rooms = new ArrayList<>();
 
         // while je nog kan lezen:
             try {
@@ -28,6 +29,27 @@ public class Main {
         for (Student student : students) {
             System.out.println(student);
         }
+
+        try {
+            BufferedReader Courseinfo =
+                    new BufferedReader (new FileReader("resources/CoursesFile.csv"));
+            while(true) {
+                String name = Courseinfo.readLine();
+                if (name == null) break;
+                courses.add(new Course(name.split(",")[0]));
+            }
+            Courseinfo.close();
+        } catch (IOException ex) {
+            System.out.println("Can't find file");
+            System.exit(1);
+        }
+
+        for (Course course : courses) {
+            System.out.println(course);
+        }
+
+
+
     }
 }
 
