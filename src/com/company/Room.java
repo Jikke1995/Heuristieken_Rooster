baef1332eeb58f1cfa892fc4f416dc5d615b7488
Jikke1995 <jikke.vdende@hotmail.com>
2016-02-25 18:50:15 +0100

    Ik heb class Room toegevoegd

---
+++ src/com/company/Room.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Room {
+    String number;
+
+    public Room(String number) {
+        this.number = number;
+    }
+
+    public String toString(){
+        return number;
+    }
+}
